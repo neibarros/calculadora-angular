d5fe6ce72b06b6cef8d4b2d2b1d6aaa7c400bfe4
Nei Barros <joselio.barros@bomcontrole.com.br>
2018-10-31 09:42:08 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		CalculadoraModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
